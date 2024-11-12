6655367bb4cd152ff1c2839cbaed8427dd6dd8ef
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2024-11-12 14:50:21 +0100

    chore: remove unneeded safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
-  safelist: [/^bg-/, /^text-/, /^p-/, /^m-/, /^w-/, /^h-/],
   theme: {
     extend: {},
   },
